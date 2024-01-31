7f541d388f8848ab071ec494ff9a4a9a540eb46b
Bean <bnasse2000@gmail.com>
2024-01-30 23:26:56 -0600

    update

--- raspberrypi/python/allowed.py
+++ raspberrypi/python/allowed.py
@@ -1,4 +1,4 @@
 import subprocess
 
-output = subprocess.check_output(["python3", "get_uid.py"])  # Assuming Python 3
+output = subprocess.check_output(["python3", "/home/pi/nfcpi/raspberrypi/python/get_uid.py"])  # Assuming Python 3
 print(output)
